d7eb70ef3bb87946c3eef5841e4d03e361c86636
alesyadubrovskaya <alexshamkina28@gmail.com>
2025-08-23 19:46:35 +0300

    The first test

--- QA_Practice/UI_elements.py
+++ QA_Practice/UI_elements.py
@@ -13,12 +13,14 @@ def test_valid_login_submit():
     driver.find_element(By.CSS_SELECTOR, "#email").send_keys('admin@admin.com')
     driver.find_element(By.CSS_SELECTOR, "#password").send_keys('admin123')
     driver.find_element(By.CSS_SELECTOR, "#submitLoginBtn").click()
+    shop_UI = driver.find_element(By.CSS_SELECTOR, "#prooood").is_displayed()
+    assert shop_UI == True
     time.sleep(5)
     driver.quit()
 
 
 def test_invalid_login_submit():
-    driver.find_element(By.CSS_SELECTOR, "#email").send_keys('admin12@admin.com')
+    driver.find_element(By.CSS_SELECTOR, "#email").send_keys('admin12admin.com')
     driver.find_element(By.CSS_SELECTOR, "#password").send_keys('a')
     driver.find_element(By.CSS_SELECTOR, "#submitLoginBtn").click()
     alert_mw = driver.find_element(By.CSS_SELECTOR, ".alert-danger").is_displayed()
@@ -31,3 +33,17 @@ def test_invalid_login_submit():
     time.sleep(5)
     driver.quit()
 
+
+def test_invalid_login_submit():
+    driver.find_element(By.CSS_SELECTOR, "#email").send_keys('admin12admin.com')
+    driver.find_element(By.CSS_SELECTOR, "#password").send_keys('a')
+    driver.find_element(By.CSS_SELECTOR, "#submitLoginBtn").click()
+    alert_mw = driver.find_element(By.CSS_SELECTOR, ".alert-danger").is_displayed()
+    alert = driver.find_element(By.CSS_SELECTOR, ".alert-danger").text
+    alert_color = driver.find_element(By.CSS_SELECTOR, ".alert-danger").value_of_css_property("background-color")
+    alert_font = driver.find_element(By.CSS_SELECTOR, ".alert-danger").value_of_css_property("font-size")
+    print(alert_color, alert_font)
+    assert alert_mw == True
+    assert alert == "Bad credentials! Please try again! Make sure that you've registered."
+    time.sleep(5)
+    driver.quit()
